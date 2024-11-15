5c5ece04baa48bbaa9fd2b34b7f69086ff695b21
Paul Röttger <paulroettger@web.de>
2024-11-15 13:13:12 +0100

    add comments and some reworks

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ struct Signal<T,U>
 where T: Default + Clone,
       U: Default + Clone
 {
+    /* Samples of a digital signal */
     pub v: Vec<Sample<T,U>>
 }
 
@@ -17,7 +18,10 @@ struct Sample<T,U>
 where T: Default + Clone,
       U: Default + Clone
 {
+    /* time compenent of the sample */
     pub t: T,
+    
+    /* measured value */
     pub x: U
 }
 
@@ -25,32 +29,34 @@ impl<T,U> Signal<T,U>
 where T: Default + Clone,
       U: Default + Clone
 {
-
+    /* create a new signal base of the time and value component */
     pub fn new(time_signal: Vec<T>, value_signal: Vec<U>) -> Signal<T,U>{
         assert_eq!(time_signal.len(), value_signal.len());
         Signal { 
-            v: value_signal.iter().zip(time_signal)
-                .map(|(x_val,t_val)| Sample{
-                                                    t:t_val, 
-                                                    x:(*x_val).clone()}
-                ).collect()
-        }
+            v: value_signal.iter().zip(time_signal.iter())
+                .map(|(x_val,t_val)| Sample{t:t_val.clone(), 
+                                                         x:x_val.clone()})
+                .collect()
+                }
     }    
     
+    /* Perform a time discrete signal fold, the signals are resampled using the time sample_time:*/
     pub fn fold_signal<'a>(&'a self, signalb: &'a Self, sample_time: &'a T) -> Option<Self>
-    where T: PartialOrd + Add<&'a T, Output = T> + Sub<&'a T, Output = T> + Div<Output=T> + Mul<&'a T, Output = T>  + TryInto<U> + TryInto<usize> + TryFrom<usize>  + From<u8> + 'a,
-          U: Mul<Output = U> + From<u8> + Sum,
-          &'a T: Sub<&'a T, Output = T> + Mul<&'a T, Output = T> + 'a,
-          &'a U: Add<U,Output = U> + Sub<&'a U, Output = U> +'a
+    where T:      PartialOrd + 
+                       Add<&'a T, Output = T> + Sub<&'a T, Output = T> + Div<Output=T> + Mul<&'a T, Output = T> +
+                       TryInto<U> + TryInto<usize> + TryFrom<usize>  + From<u8> + 'a,
+              U:      Mul<Output = U> + From<u8> + Sum,
+              &'a T: Sub<&'a T, Output = T> + Mul<&'a T, Output = T> + 'a,
+              &'a U: Add<U,Output = U> + Sub<&'a U, Output = U> +'a
     {
-
-        let t_min_siga = &self.v.first()?.t;
-        let t_min_sigb = &signalb.v.first()?.t;
-
+        
         if *sample_time <= 0_u8.into(){
             return None;
         }
 
+        /* Resample the input signals and add zeros to ease zipping */
+        let t_min_siga = &self.v.first()?.t;
+        let t_min_sigb = &signalb.v.first()?.t;
         let sig_a;
         let sig_b;
         let len_diff;
@@ -66,28 +72,35 @@ where T: Default + Clone,
             sig_b = [vec![0_u8.into();len_diff + sig_a.len() - 1], signalb.resample(sample_time)].concat();
         }
         
+        /* Generate an vector for the results */
         let len_result = sig_b.len();
         let len_a = sig_a.len();
         let mut result = vec![0_u8.into(); len_result];
 
-
+        /* Perfor the signal fold */
         for (index, sample) in result.iter_mut().enumerate(){
             *sample = sig_a.iter()
-                        .zip(sig_b.iter().skip(index).take(len_a).rev())
-                        .map(|(a,b)| a.clone()*b.clone()).sum();
+                                    .zip(sig_b.iter().skip(index).take(len_a).rev())
+                                    .map(|(a,b)| a.clone()*b.clone())
+                                    .sum();
 
         }
 
-        Some(Signal::new((0..len_result).into_iter().map(|i| match T::try_from(i){Ok(i_ok) => i_ok*sample_time, _=> 0_u8.into()}).collect(),
+		/* Generate time signal and return result */
+        Some(Signal::new((0..len_result).into_iter()
+                                                     .map(|i| 
+                                                             match T::try_from(i){ Ok(i_ok) => i_ok*sample_time,
+                                                                                                       _ => 0_u8.into()})
+                                                     .collect(),
             result))
     }
-    
-
+        
+    /* Resample a signal with a given sampletime */
     fn resample<'a>(&'a self, sampletime: &'a T) -> Vec<U>
-    where T: PartialOrd + Add<&'a T, Output = T> + Sub<&'a T, Output = T> + Div<Output=T> + TryInto<U> +'a,
-          U: Mul<Output = U> + From<u8>,
-          &'a T: Sub<&'a T, Output = T> + 'a,
-          &'a U: Add<U,Output = U> + Sub<&'a U, Output = U> + 'a
+    where T:      PartialOrd + Add<&'a T, Output = T> + Sub<&'a T, Output = T> + Div<Output=T> + TryInto<U> +'a,
+              U:      Mul<Output = U> + From<u8>,
+              &'a T: Sub<&'a T, Output = T> + 'a,
+              &'a U: Add<U,Output = U> + Sub<&'a U, Output = U> + 'a
     {
         let first_sample = match self.v.first() {Some(s) => s, _ => return Vec::new()};
 
@@ -98,11 +111,13 @@ where T: Default + Clone,
         for i in 1..self.v.len(){
             while new_t < self.v[i].t{
                 new_t = new_t + sampletime;
-                let new_x = &self.v[i-1].x+(&self.v[i].x-&self.v[i-1].x)*(((new_t.clone()-&self.v[i-1].t))/(&self.v[i].t-&self.v[i-1].t)).try_into().unwrap_or(1_u8.into());
+                let new_x = &self.v[i-1].x +
+                                   (&self.v[i].x-&self.v[i-1].x) *
+                                    (((new_t.clone()-&self.v[i-1].t))/(&self.v[i].t-&self.v[i-1].t))
+                                    .try_into().unwrap_or(1_u8.into());
                 result.push(new_x);
             }
         }
-
         result
     }
 
